8613e8a29a797ef13f282eb6f259170066cc0fc5
Emily de França <emydefranca@gmail.com>
2026-03-07 18:17:21 -0300

    Initial commit

---
+++ exercicios/lista01/ex01.py
@@ -0,0 +1,4 @@
+#Faça um algoritmo que leia dois números e mostre o produto desses números.
+num1 = int( input("Digite o primeiro valor:"))
+num2 = int( input("Digite o segundo valor:"))
+print("O resultado é: ", num1 * num2)
