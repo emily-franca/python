95de408952ca13efc95b2508a25235027b5400ad
Emily de França <emydefranca@gmail.com>
2026-05-10 20:31:11 -0300

    projeto 01

---
+++ udemy/bandNameGenerator.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator!")
+cityName = input("What's the name of the city you grew up in?\n")
+fruitName = input("What's your favourite fruit?\n")
+print(f"Your band name could be {cityName+" n' "+fruitName}")
